cafb3204cfd0ed59581474e70c7d1d634eab36d8
lillee205 <leelillian205@gmail.com>
2021-07-15 15:09:08 -0700

    Here, you can type a commit message which will explain what changes you made

--- app.py
+++ app.py
@@ -1,26 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 import random
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'e7d981238199178c1abd8bc12dda98bd600d4a7cb7e3ecb2'
+db = SQLAlchemy(app)
+
 # This list will contain all my warbles/tweets! I can later use this to "load" them into the page
-warbleList = []
+class Warble(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String, nullable = False)
+    msg = db.Column(db.String, nullable = False)
+
+    def __repr__(self):
+        return "Author: {author}\nMsg: {msg}".format(
+            author=self.author, msg=self.msg)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    global warbleList
     if request.method == "POST" and request.form.get("msg") != "":
-        newWarble = {
-            "author": "wart_is_law", #for now, we'll just say every author is wally :)
-            "msg" : request.form.get("msg") if random.random() <= 0.75 else pig_translate(request.form.get("msg"))
-        }
-        warbleList.append(newWarble)
+        newWarble = Warble(
+            author= "wart_is_law",  # for now, we'll just say every author is wally :)
+            msg =  request.form.get("msg") if random.random() <= 0.75 else pig_translate(request.form.get("msg"))
+        )
+        db.session.add(newWarble)
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template("index.html", warbleList = warbleList) #then, I will pass in my warbleList into the rendering of the HTML pg
+    # then, I will pass in my warbleList into the rendering of the HTML pg
+    warbleList = Warble.query.order_by(Warble.id.desc()).all()
+    return render_template("index.html", warbleList= warbleList)
+
 
 def pig_translate(S):
     retStr = ''
-    V = ['a','A','e','E','i','I','o','O','u','U']
+    V = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
     currFront = S[0]
     seen_vowel = False
     for i in range(len(S)):
@@ -31,7 +48,7 @@ def pig_translate(S):
                 retStr += currFront.lower() + 'ay '
         elif i == len(S)-1:
             if currFront in V:
-                retStr += S[i]+ 'yay'
+                retStr += S[i] + 'yay'
             else:
                 retStr += S[i] + currFront.lower() + 'ay'
         else:
@@ -52,6 +69,9 @@ def pig_translate(S):
                         retStr += S[i]
     return retStr
 
-if __name__ == "__main__":
-    app.run(debug = True)
 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    db.create_all()
--- model.py
+++
@@ -1,8 +0,0 @@
-class Warble:
-    def __init__(self, author, msg):
-        self.author = author
-        self.msg = msg
-
-    def __repr__(self):
-        return "Author: {author}\nMsg: {msg}".format(
-            author=self.author, msg=self.msg)
